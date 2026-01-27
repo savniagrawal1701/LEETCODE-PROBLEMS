4df808d8e2511a80f18f5fd709245a057ee9aca2
Savni agrawal <savniagrawal794@gmail.com>
2026-01-27 15:17:07 +0530

    Add files via upload

---
+++ pra.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> ans;
+        int n=nums.size();
+        vector<int> hash(n + 1, 0);
+        for(int i =0;i<n;i++){
+            hash[nums[i]]+=1;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (hash[i] == 0) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
