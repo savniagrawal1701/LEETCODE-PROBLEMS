7b8fe17a67bda97a6fc4d9b914d3804e8845c7c6
Savni agrawal <savniagrawal794@gmail.com>
2026-01-27 16:28:15 +0530

    Add files via upload

---
+++ pra.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+    //time complexity will be n*n
+    vector<int> ans ;
+    int n =nums.size();
+    for(int i =0;i<n;i++){
+        int count=0;
+        for(int j=0;j<n;j++){
+            if(nums[j]<nums[i]){
+                count++;
+            }
+        }
+        ans.push_back(count);
+    }
+        return ans;
+    }
+};
