ce24e1391627b5e9c3ad1a74198f16fe4c710ad3
Savni agrawal <savniagrawal794@gmail.com>
2026-02-01 10:47:53 +0530

    Add files via upload

---
+++ pra.cpp
@@ -0,0 +1,9 @@
+//1feb
+//Divide an Array Into Subarrays With Minimum Cost I
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        sort(nums.begin()+1,nums.end());
+        return nums[0]+nums[1]+nums[2];
+    }
+};
