9bf91a915bf18856f0b346040495af3414a094b3
Savni agrawal <savniagrawal794@gmail.com>
2026-02-15 21:26:09 +0530

    Add files via upload

--- pra.cpp
+++ pra.cpp
@@ -1,12 +1,32 @@
-//2124. Check if All A's Appears Before All B's
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    bool checkString(string s) {
-        int n =s.size();
-        for(int i =0;i<=n-2;i++){
-           if(s[i]=='b'&& s[i+1]=='a'){
-            return false;
-           }
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        ListNode* temp = head;
+        if(head==nullptr) return true;
+        while (temp != nullptr) {
+            st.push(temp->val);
+            temp = temp->next;
+        }
+        ListNode* temp1 = head;
+        while (temp1 != nullptr) {
+            if (temp1->val == st.top()) {
+                temp1 = temp1->next;
+                st.pop();
+               
+            } else {
+                return false;
+            }
         }
         return true;
     }
