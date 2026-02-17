7c49102a62d7c6ccd63a5b9104f20d486e2f2f88
Savni agrawal <savniagrawal794@gmail.com>
2026-02-17 19:33:09 +0530

    Add files via upload

---
+++ pra.cpp
@@ -0,0 +1,13 @@
+
+
+class Solution {
+public:
+    int pivotInteger(int n) {
+        if (n==1) return 1;
+        else if(n==8) return 6;
+        else if(n==49) return 35;
+        else if(n==288) return 204;
+        
+        return  -1;
+    }
+};
