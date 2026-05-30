e22577f881dc6412231a4f0fa6267cf580f85a0a
Savni agrawal <savniagrawal794@gmail.com>
2026-05-30 22:06:28 +0530

    Update Make Array Non-decreasing.cpp

--- Make Array Non-decreasing.cpp
+++ Make Array Non-decreasing.cpp
@@ -17,3 +17,16 @@ else{
         return count+1;
     }
 };
+
+//apporach 2
+class Solution {
+public:
+    int maximumPossibleSize(vector<int>& nums) {
+        stack<int>st;
+        for(int i =0;i<nums.size();i++){
+            if(st.empty() || st.top()<=nums[i]) st.push(nums[i]);
+            else continue;
+        }
+        return st.size();
+    }
+};
