a990ded68a051f1946b0e10bc3709c9c30455e8e
Savni agrawal <savniagrawal794@gmail.com>
2026-06-02 19:41:03 +0530

    Create 930 Binary Subarrays With Sum.cpp

---
+++ 930 Binary Subarrays With Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+ int atMost(vector<int>& nums, int k) {
+        // If k is negative, no such subarrays exist
+        if (k < 0) return 0;
+
+        int left = 0;
+        int sum = 0;
+        int count = 0;
+
+        // Traverse the array using right pointer
+        for (int right = 0; right < nums.size(); right++) {
+            // Add current element to sum
+            sum += nums[right];
+
+            // Shrink the window from the left if sum exceeds k
+            while (sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            // Add the number of valid subarrays ending at right
+            count += (right - left + 1);
+        }
+
+        return count;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+
+    }
+};
