bd33a491c09b29ac30202ff830305c1042b2b2ff
Savni agrawal <savniagrawal794@gmail.com>
2026-06-19 07:57:57 +0530

    Create 1732 Find the Highest Altitude.cpp

---
+++ 1732 Find the Highest Altitude.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int sum=0;
+        int maxi=0;
+        for(int i =0;i<gain.size();i++){
+sum=sum+gain[i];
+maxi=max(sum,maxi);
+
+        }
+        return maxi;
+    }
+};
