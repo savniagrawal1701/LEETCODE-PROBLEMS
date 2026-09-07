20635ff52a03932752e6cf547d0c690cd9abc911
Savni agrawal <savniagrawal794@gmail.com>
2026-09-07 22:21:39 +0530

    Add files via upload

--- pra.cpp
+++ pra.cpp
@@ -1,30 +1,24 @@
+//HOURGLASS
 class Solution {
 public:
-    int firstStableIndex(vector<int>& nums, int k) {
-       int i =0;
-    //    int maxi=-1;             //logic at last
-    //    int mini=INT_MAX;
-       while(i<nums.size()){
-          int maxi = nums[0]; 
-            int mini = nums[i]; 
+    int maxSum(vector<vector<int>>& grid) {
+         int m = grid.size();
+        int n = grid[0].size();
+        int max_sum = 0;
 
-       for (int j=0;j<=i;j++){
-        maxi=max(maxi, nums[j]);
-       }
-         for (int j=i;j<nums.size();j++){
-        mini=min(mini, nums[j]);
-       
-       }
-       
-       if(maxi-mini<=k) return i ;
-    i++;
-       }
-      return -1;
+        
+        for (int i = 0; i < m - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                
+                int current_sum = grid[i][j]   + grid[i][j+1]   + grid[i][j+2] 
+                                               + grid[i+1][j+1]                
+                                + grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]; 
+                
+                
+                max_sum = max(max_sum, current_sum);
+            }
+        }
+
+        return max_sum;
     }
 };
-
-//The problem with keeping the initialization at the very start is that mini and maxi never forget the elements from previous rounds, even when they shouldn't.
-//When you move i forward, you are changing the specific windows of the array you want to check:
-//The maxi window grows (0 to i) \(\rightarrow \) This part actually works fine if you keep it outside.
-//The mini window shrinks (i to the end) \(\rightarrow \) This is where the logic breaks.
-//The Shrinking Window ProblemAs i increases, elements are dropped from the beginning of the mini loop. If a dropped element happened to be the smallest number in the entire array, mini will permanently stick to that old, dropped value. It can never increase because the min() function only lets it stay the same or get smaller.
